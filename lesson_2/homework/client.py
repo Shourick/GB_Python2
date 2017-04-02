4df092a470d0b41884133bb011bed4d104dc9321
Alexander S.Naoumov <shourick@yandex.ru>
2017-04-03 00:11:48 +0300

    1. Переделал сервер 2. Переделал клиент 3. Добавил сканирование диапозона портов для соединения с сервером и считыватель клавиатуры

--- lesson_2/homework/client.py
+++ lesson_2/homework/client.py
@@ -1,20 +1,61 @@
 
-# Клиент игры "Запоминалка"
-
 import socket
+import keyboard
 
-HOST, PORT = 'localhost', 9999
+__author__ = 'Наумов Александр Сергеевич'
 
-print('Клиент запущен')
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
 
-sock.sendall(bytes('I_WANNA_PLAY', 'utf-8'))
+def find_port(server, port_range, response):
+    for port in port_range:
+        _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        _socket.settimeout(0.001)
+        try:
+            _socket.connect((server, port))
+            _socket.send(bytes('Test connection', 'utf-8'))
+            _response = _socket.recv(1024)
+        except (socket.timeout, OSError):
+            _socket.close()
+        else:
+            if str(_response, 'utf-8') == response:
+                return port
 
-recvd = str(sock.recv(1024), 'utf-8')
 
-print(recvd)
+def fill_buffer(event):
+    if len(buffer) < 10:
+        buffer.append(event.name)
+    else:
+        keyboard.unhook(fill_buffer)
+        keyboard.hook(keyboard_event)
 
-sock.close()
 
-# __add__  ->  +
+def keyboard_event(event):
+    if not buffer and event.name == 's':
+        keyboard.unhook(keyboard_event)
+        keyboard.hook(fill_buffer)
+    elif event.name == 'esc':
+        buffer[0] = False
+
+
+HOST = 'localhost'
+# PORT = 9999
+print('Try to connect to server')
+PORT = find_port(HOST, range(9000, 10000), 'Connection is OK')
+if PORT:
+    buffer = []
+    response = []
+    keyboard.hook(keyboard_event)
+
+    while (not buffer or buffer[0]) and len(buffer) < 10:
+        pass
+    if buffer[0]:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((HOST, PORT))
+        print('Клиент запущен')
+        sock.send(bytes(' '.join(buffer), 'utf-8'))
+        response.append(str(sock.recv(1024), 'utf-8'))
+
+        print(response)
+
+        sock.close()
+else:
+    print('Can not connect to {}'.format(HOST))
--- lesson_2/homework/server.py
+++ lesson_2/homework/server.py
@@ -1,22 +1,23 @@
 
-# Сервер игры "Запоминалка"
+# Сервер платёжного терминала
 
 import socketserver
-import random
+
+__author__ = 'Наумов Александр Сергеевич'
+
 
 class MemTCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
-        self.data = self.request.recv(1024).decode()
+        self.data = self.request.recv(1024).decode('utf-8')
         print("Клиент {} сообщает {}".format(self.client_address[0], self.data))
 
-        if self.data == "I_WANNA_PLAY":
-            nums = random.sample(range(1,100), 18) * 2
-            random.shuffle(nums)
-            s_nums = [str(n) for n in nums]
-            s_nums = ';'.join(s_nums)
-
-            self.request.sendall(bytes('NUMS;'+s_nums, 'utf-8'))
+        if self.data == 'Test connection':
+            self.request.send(bytes('Connection is OK', 'utf-8'))
+        elif self.data.startswith('s'):
+            self.request.send(
+                bytes('{} was received'.format(self.data), 'utf-8')
+            )
         else:
             print('Неизвестный запрос')    
 
@@ -24,7 +25,7 @@ class MemTCPHandler(socketserver.BaseRequestHandler):
 HOST, PORT = 'localhost', 9999
 
 server = socketserver.TCPServer((HOST, PORT), MemTCPHandler)  
-print('Сервер игры запущен')
+print('Testing server is running')
 
 server.serve_forever()
 
