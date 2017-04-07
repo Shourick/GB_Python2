c677fd406b21dbf87fa08a63d0937b649522e8ca
Alexander S.Naoumov <shourick@yandex.ru>
2017-04-07 19:44:37 +0300

    # Выбрать платёжные транкции для указанного терминала (id)     # за указанный период (от моего рождения до текущего момента)     print(db.select_transaction(terminal_id=1))     # За указанный период (datetime) по данным платёжных транзакций     # сформировать выборку какая сумма должна быть перечислена каждой     # фирме-партнёру (можно не учитывать тех партнёров,     # для которых нет платежей).     print(db.total_balance())     # По данным платёжных транзакций сформировать выборку с указанием,     # какая сумма прошла через каждый терминал за указанный период.     print(db.balance(model=Terminal, ref_model=Transaction))     #  *Дополнительно.* По каждому терминалу сформировать отчет,     # где отражаются временные периоды в течение дня (0-6, 6-12, 12-18, 18-24)     # и количество транзакций в каждый временной период.     time_step = 6     for i in range(0, 24, time_step):         print('Period {} - {}'.format(i, i + time_step))         print(             list(                 Terminal.select(Terminal.name).annotate(                     Transaction, fn.Count(Transaction.id).alias('count')                 ).where(                     Transaction.date_time.hour >= i,                     Transaction.date_time.hour < (i + time_step)                 ).dicts()             )         )

--- lesson_3/homework/models.py
+++ lesson_3/homework/models.py
@@ -7,6 +7,8 @@ from peewee import *
 BASE_DIR = os.path.dirname(__file__)
 DB_FILE_NAME = os.path.join(BASE_DIR, 'db.sqlite3')
 DATA_BASE = SqliteDatabase(DB_FILE_NAME)
+START_DATE = datetime.datetime(1971, 12, 25)
+END_DATE = datetime.datetime.now()
 
 
 class MyModel(Model):
@@ -31,7 +33,7 @@ class Term(MyModel):
 
 
 class Deal(MyModel):
-    company = ForeignKeyField(Company, related_name='balance')
+    company = ForeignKeyField(Company, related_name='deal')
     date_of_payment = DateField()
     term = ForeignKeyField(Term)
     amount = FloatField()
@@ -45,8 +47,8 @@ class Terminal(MyModel):
 
 
 class Transaction(MyModel):
-    company = ForeignKeyField(Company)
-    terminal = ForeignKeyField(Terminal)
+    company = ForeignKeyField(Company, related_name='transactions')
+    terminal = ForeignKeyField(Terminal, related_name='transactions')
     amount = FloatField()
     date_time = DateTimeField(default=datetime.datetime.now())
 
@@ -57,10 +59,10 @@ class PaymentType(MyModel):
 
 
 class Payment(MyModel):
-    company = ForeignKeyField(Company)
+    company = ForeignKeyField(Company, related_name='payments')
     payment_type = ForeignKeyField(PaymentType)
     date_time = DateTimeField(default=datetime.datetime.now())
-    deal = ForeignKeyField(Deal)
+    deal = ForeignKeyField(Deal, related_name='payments')
     amount = FloatField()
 
 
@@ -108,57 +110,75 @@ class DbWorks:
 
     @staticmethod
     def select_transaction(
-            terminal_id=None,
-            start_date=datetime.datetime(1971, 12, 25),
-            end_date=datetime.datetime.now()
+            terminal_id=None, start_date=START_DATE, end_date=END_DATE
     ):
+        '''Makes the report about transaction for the specified terminal 
+        within the term if the terminal is not specified,
+         then makes the report for all terminals 
+        
+        :param terminal_id: Id of a terminal
+        :param start_date: Start date of the term
+        :param end_date: End date of the term
+        :return: Query from the Transaction table
+        '''
         if terminal_id:
-            return Transaction.select('*').where(
+            return list(Transaction.select().where(
                 Transaction.terminal == terminal_id,
                 Transaction.date_time <= end_date,
                 Transaction.date_time >= start_date
-            ).execute()
-        return Transaction.select().where(
+            ).dicts())
+        return list(Transaction.select().where(
             Transaction.date_time <= end_date,
             Transaction.date_time >= start_date
-        ).execute()
+        ).dicts())
 
     @staticmethod
     def balance(
-            model,
-            company=None,
-            start_date=datetime.datetime(1971, 12, 25),
-            end_date=datetime.datetime.now()
+            model, ref_model, model_id=None,
+            start_date=START_DATE, end_date=END_DATE
     ):
-        if company:
+        '''Calculates the operational balance for the specified model 
+        within the term. If the model_id is not specified, then calculates 
+        the balance for all models
+        
+        :param model: The reference table of balanced items  
+        :param ref_model: The table name where operational data are stored
+        :param model_id: Id of the balanced item
+        :param start_date: Start date of the term
+        :param end_date: End date of the term
+        :return: The list of the dictionary like this:
+        [
+            {'name': <company name_1>, 'amount': <amount_1>},
+            ...
+            {'name': <company name_n>, 'amount': <amount_n>}
+        ]
+        '''
+        if model_id:
             return list(
-                model.select(
-                    Company.name, fn.SUM(model.amount)
-                ).join(Company).group_by(model.company).where(
-                    model.company == company,
-                    model.date_time <= end_date,
-                    model.date_time >= start_date
+                model.select(model.name).annotate(
+                    ref_model, fn.SUM(ref_model.amount)).where(
+                    model.id == model_id,
+                    ref_model.date_time <= end_date,
+                    ref_model.date_time >= start_date
                 ).dicts()
             )
         return list(
-            model.select(
-                Company.name, fn.SUM(model.amount)
-            ).join(Company).group_by(model.company).where(
-                model.date_time <= end_date, model.date_time >= start_date
-            ).dicts()
-        )
+                model.select(model.name).annotate(
+                    ref_model, fn.SUM(ref_model.amount)).where(
+                    ref_model.date_time <= end_date,
+                    ref_model.date_time >= start_date
+                ).dicts()
+            )
 
-    def company_balance(
-            self,
-            company=None,
-            start_date=datetime.datetime(1971, 12, 25),
-            end_date=datetime.datetime.now()
+    def total_balance(
+            self, model=Company, model_id=None,
+            start_date=START_DATE, end_date=END_DATE
     ):
         transaction_balance = self.balance(
-            Transaction, company, start_date, end_date
+            model, Transaction, model_id, start_date, end_date
         )
         payment_balance = self.balance(
-            Payment, company, start_date, end_date
+            model, Payment, model_id, start_date, end_date
         )
         for transaction in transaction_balance:
             company = transaction['name']
@@ -168,11 +188,10 @@ class DbWorks:
                     transaction = None
             if transaction:
                 payment_balance.append(transaction)
-        return payment_balance
+        return dict((item['name'], item['amount']) for item in payment_balance)
 
     def insert_replace(self, row_list, model):
         """Inserts or replaces row in the table
-
         :param row_list: The list of the rows
         :param model: The class of a table in the database
         """
@@ -218,3 +237,32 @@ class DbWorks:
 
 
 DbWorks.max_sql_variables()
+if __name__ == '__main__':
+    db = DbWorks()
+    # Выбрать платёжные транкции для указанного терминала (id)
+    # за указанный период (от моего рождения до текущего момента)
+    print(db.select_transaction(terminal_id=1))
+    # За указанный период (datetime) по данным платёжных транзакций
+    # сформировать выборку какая сумма должна быть перечислена каждой
+    # фирме-партнёру (можно не учитывать тех партнёров,
+    # для которых нет платежей).
+    print(db.total_balance())
+    # По данным платёжных транзакций сформировать выборку с указанием,
+    # какая сумма прошла через каждый терминал за указанный период.
+    print(db.balance(model=Terminal, ref_model=Transaction))
+    #  *Дополнительно.* По каждому терминалу сформировать отчет,
+    # где отражаются временные периоды в течение дня (0-6, 6-12, 12-18, 18-24)
+    # и количество транзакций в каждый временной период.
+    time_step = 6
+    for i in range(0, 24, time_step):
+        print('Period {} - {}'.format(i, i + time_step))
+        print(
+            list(
+                Terminal.select(Terminal.name).annotate(
+                    Transaction, fn.Count(Transaction.id).alias('count')
+                ).where(
+                    Transaction.date_time.hour >= i,
+                    Transaction.date_time.hour < (i + time_step)
+                ).dicts()
+            )
+        )
